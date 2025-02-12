501b056c5fc16c999455eb61db9f54876e856cff
Erik Chable <erikchable88@gmail.com>
2025-02-12 14:55:09 -0600

    changes

--- includes/app.php
+++ includes/app.php
@@ -1,12 +1,15 @@
 <?php
 
-use Model\ActiveRecord;
+
 
 require __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeload();
-
 require 'config/database.php';
 require 'funciones.php';
+
+$db = conectarDB();
+
+use Model\ActiveRecord;
 
 ActiveRecord::setDB($db);
--- includes/config/database.php
+++ includes/config/database.php
@@ -1,15 +1,26 @@
 <?php
 
-$db = mysqli_connect(
-    $_ENV['DB_HOST'],
-    $_ENV['DB_USER'],
-    $_ENV['DB_PASS'],
-    $_ENV['DB_NAME']
-);
+function conectarDB(): mysqli
+{
+    $db = new mysqli(
+        $_ENV['DB_HOST'],
+        $_ENV['DB_USER'],
+        $_ENV['DB_PASS'],
+        $_ENV['DB_NAME']
+    );
 
-$db->set_charset("utf8");
+    if (!$db) {
+        echo "Error, no se pudo conectar a la DB";
+        exit;
+    }
 
-if (!$db) {
-    echo "Error, no se pudo conectar a la DB";
-    exit;
+    return $db;
 }
+
+// $db = mysqli_connect(
+//     $_ENV['DB_HOST'],
+//     $_ENV['DB_USER'],
+//     $_ENV['DB_PASS'],
+//     $_ENV['DB_NAME']
+// );
+// $db->set_charset("utf8");
