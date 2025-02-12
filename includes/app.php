4e0a6119c773064afe83397e9fce8ab8ed940d64
Erik Chable <erikchable88@gmail.com>
2025-02-12 16:27:05 -0600

    changes

--- includes/app.php
+++ includes/app.php
@@ -1,15 +1,13 @@
 <?php
 
-
+use Model\ActiveRecord;
 
 require __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->safeload();
-require 'config/database.php';
+$dotenv->load();
 require 'funciones.php';
+require 'config/database.php';
 
 $db = conectarDB();
-
-use Model\ActiveRecord;
 
 ActiveRecord::setDB($db);
