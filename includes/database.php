de07e87f9638fc431445d3447c078b79bdbf9f06
Erik Chable <erikchable88@gmail.com>
2025-02-13 20:15:07 -0600

    changes

--- includes/database.php
+++ includes/database.php
@@ -4,10 +4,7 @@ function conectarDB(): mysqli
 {
     $db = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_NAME']);
 
-    var_dump($_ENV);
-    var_dump(getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PASS'), getenv('DB_NAME'));
-
-
+    $db->set_charset("utf8");
     if (!$db) {
         echo "Error, no se pudo conectar a la DB";
         exit;
